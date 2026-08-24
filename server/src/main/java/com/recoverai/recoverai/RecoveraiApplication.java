f1ae0ad09098751fee2f6ca627080797f14d0c12
Rudra Gupta <rudragupta200408@gmail.com>
2026-08-24 19:14:56 +0530

    Initialize Spring Boot backend

---
+++ server/src/main/java/com/recoverai/recoverai/RecoveraiApplication.java
@@ -0,0 +1,13 @@
+package com.recoverai.recoverai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecoveraiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecoveraiApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/recoverai/recoverai/RecoveraiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recoverai.recoverai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecoveraiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
