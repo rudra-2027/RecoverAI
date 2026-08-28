9a6515c5d794592332f82fec8297b445ec689254
Rudra Gupta <rudragupta200408@gmail.com>
2026-08-28 22:44:24 +0530

    Updated Docker Files For Deplyoment

--- server/src/main/java/com/recoverai/recoverai/service/impl/SchedulerServiceImpl.java
+++ server/src/main/java/com/recoverai/recoverai/service/impl/SchedulerServiceImpl.java
@@ -2,11 +2,13 @@ package com.recoverai.recoverai.service.impl;
 
 import com.recoverai.recoverai.dto.AnalysisResult;
 import com.recoverai.recoverai.service.SchedulerService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 
 @Service
+@Slf4j
 public class SchedulerServiceImpl
         implements SchedulerService {
 
@@ -17,11 +19,15 @@ public class SchedulerServiceImpl
         if ("TD".equals(
                 analysis.classification())) {
 
-            return LocalDateTime.now()
+            LocalDateTime scheduledAt = LocalDateTime.now()
                     .plusHours(4);
+            log.info("Scheduled recovery retry for classification={} at={}", analysis.classification(), scheduledAt);
+            return scheduledAt;
         }
 
-        return LocalDateTime.now()
+        LocalDateTime scheduledAt = LocalDateTime.now()
                 .plusHours(24);
+        log.info("Scheduled recovery retry for classification={} at={}", analysis.classification(), scheduledAt);
+        return scheduledAt;
     }
 }
